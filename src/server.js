49f2b8ded17b45535c4cea3d86b21db52e81ba04
Yevhen <Artuikhov@gmail.com>
2026-03-16 00:50:59 +0200

    finish express homework

--- src/server.js
+++ src/server.js
@@ -1,11 +1,47 @@
-// src/server.js
 import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
+import pinoHttp from 'pino-http';
+
+dotenv.config();
 
 const app = express();
-const message = 'Hello world';
+const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
-app.use(cors()); // Дозволяє запити з будь-яких джерел
+app.use(pinoHttp());
 
-console.log(message);
+app.get('/notes', (req, res) => {
+  res.status(200).json({
+    message: 'Retrieved all notes',
+  });
+});
+
+app.get('/notes/:noteId', (req, res) => {
+  const { noteId } = req.params;
+
+  res.status(200).json({
+    message: `Retrieved note with ID: ${noteId}`,
+  });
+});
+
+app.get('/test-error', (req, res) => {
+  throw new Error('Simulated server error');
+});
+
+app.use((req, res) => {
+  res.status(404).json({
+    message: 'Route not found',
+  });
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message: err.message,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
